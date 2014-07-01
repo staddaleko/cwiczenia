d292b1e312697afb74bbc67ce77c021b7415b14c
staddaleko <staddaleko@gmail.com>
2014-07-01 22:20:35 +0200

    zaczynamy powoli

---
+++ WszystkieCwiczenia/WszystkieCwiczenia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zajecia01LAB01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, tmp;
+            int c, d;
+            Console.Write("Podaj wartość zmiennej a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj wartość zmiennej b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            tmp = a;
+            a = b;
+            b = tmp;
+            Console.WriteLine("a={0}, b={1}", a, b);
+            Console.ReadKey();
+            c = Convert.ToInt32(a) - 15; d = Convert.ToInt32(b) + 20;
+            c ^= d;
+            d ^= c;
+            c ^= d; //działa drugi sposób zamiany wartości, ale tylko całkowitych liczb
+            Console.WriteLine("c={0}, d={1}", c, d);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WszystkieCwiczenia/Zajecia01LAB01cw02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, v;
+            Console.WriteLine("Podaj wartość zmiennej x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj wartość zmiennej y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj wartość zmiennej z: ");
+            z = Convert.ToDouble(Console.ReadLine());
+
+            v = (4 * x - y) / (3 * (z * z + 1));
+
+            Console.WriteLine("wartość wyrażenia v = (4 * x - y) / (3 * (z * z + 1)) wynosi: \n{0}.", v);
+            Console.ReadKey();
+        }
+    }
+}
