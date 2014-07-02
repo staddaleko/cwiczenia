eca511e7bf8a836a5c3a6d7e4458623e1a21288a
staddaleko <staddaleko@gmail.com>
2014-07-02 21:58:32 +0200

    LAB03cw01 nie działa :(

---
+++ WszystkieCwiczenia/Zajecia03LABcw01/OsobaDlakonsoli.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zajecia03LABcw01
+{
+    class OsobaDlakonsoli
+    {
+        public OsobaDlaKonsoli() 
+        { 
+            Console.Write("Podaj imię: ");  
+            string imie = Console.ReadLine(); 
+            Console.Write("Podaj nazwisko: "); 
+            string nazwisko = Console.ReadLine();  
+            int numerEwidencyjny; 
+            do 
+            {  
+                Console.Write("Podaj numer ewidencyjny: ");  
+            }  
+            while (!int.TryParse(Console.ReadLine(), out numerEwidencyjny)); 
+            int rokUrodzenia; 
+            do
+            { 
+                Console.Write("Podaj rok urodzenia: ");  
+            }  
+            while (!int.TryParse(Console.ReadLine(), out rokUrodzenia)); 
+            Console.WriteLine("Podaj adres zamieszkania:"); 
+            AdresDlaKonsoli adr = new AdresDlaKonsoli(); 
+            this.Osoba = new Osoba(numerEwidencyjny, rokUrodzenia, imie,     nazwisko, adr.Adres); 
+        }  
+
+        public OsobaDlaKonsoli(Osoba osoba)
+        {  
+             this.Osoba = osoba; 
+        }
+        OsobaDlaKonsoli os1 = new OsobaDlaKonsoli(); 
+        Console.WriteLine(); 
+        os1.ZmienAdres(); 
+        Console.WriteLine(); 
+        os1.ZmienNazwisko(); 
+        Console.WriteLine("\n***************\n"); 
+        os1.WypiszOsobe(); 
+        Console.WriteLine("\n***************\n");
+        Osoba osoba2 = new Osoba(123, 1990, "Anna", "Kowalska", 12, 23,      "Kwiatowa", "97-350", "Piotrków Tryb."); 
+        os1.Osoba = osoba2; 
+        os1.ZmienAdres(); 
+        Console.WriteLine();  
+        os1.ZmienNazwisko(); 
+        Console.WriteLine("\n***************\n"); 
+        os1.WypiszOsobe(); 
+        Console.ReadKey();
+    }
+}
---
+++ WszystkieCwiczenia/Zajecia03LABcw05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Twoja firma opracowuje program kadrowy dla pewnej firmy. Twoim zadaniem jest dokończenie i przetestowanie klasy Osoba. Do klasy Osoba oraz Adres musisz dodać właściwości przy pomocy których będzie można uzyskać dostęp do pól klasy. Zwróć uwagę, że wartość polu numerEwidencyjny klasy Osoba jest nadawana w momencie tworzenia obiektu i później ta wartość nie może być zmieniona. Zabezpiecz, aby nie można było utworzyć osoby, która się jeszcze nie urodziła. Oprócz roku urodzenia również w programie kadrowym potrzebny będzie wiek osoby. Dodaj również klasy, które będą pośredniczyć w interakcji między klasami Osoba i Adres a konsolą. 
+ */
+
+namespace Zajecia03LABcw05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
